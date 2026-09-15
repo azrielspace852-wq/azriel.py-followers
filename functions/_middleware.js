e345f52eeff834b1f20b475c887a438e39a2d9eb
azrielspace852-wq <azrielspace852@gmail.com>
2026-09-15 13:30:36 +0700

    Delete functions directory

--- functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    // CORS headers buat semua response
-    const response = await context.next();
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-    return response;
-}
